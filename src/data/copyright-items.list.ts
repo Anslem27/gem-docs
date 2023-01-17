e3dfd157dde7851833133b09aeb3de3a5349babd
Anslem <89728185+Anslem27@users.noreply.github.com>
2023-01-17 19:05:21 +0300

    updated copyrights

--- src/data/copyright-items.list.ts
+++ src/data/copyright-items.list.ts
@@ -2,8 +2,8 @@ import type { CopyrightItem } from 'src/models/copyright-item.interface';
 
 export const copyrightItemList: CopyrightItem[] = [{
 	prefix: '©',
-	href: 'https://github.com/Anslem27',
-	text: 'Anslem Seguya',
+	href: 'https://github.com/VanVictor20',
+	text: 'Wandulu Victor',
 	suffix: '• All rights reserved',
 }, {
 	prefix: 'All music art used on this website is licensed and owned by the authors and artists and is in no way affiliated with the Gem app. ',
